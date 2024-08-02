ffb2b4eec1d3b50915a5dadc9b2d15f26ab1f8c1
Majid Namnabat <maj.nam@gmail.com>
2024-08-02 20:20:20 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'ac3b0d69-f5f2-4624-8244-e08279d2dbf6';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
